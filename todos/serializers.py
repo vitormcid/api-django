2a289b655209de326fa06510fb9ac0bbc375fb11
Vitor <cid.vitor@hotmail.com>
2019-12-30 21:34:42 -0300

    create a basic logic API

---
+++ todos/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+ 
+from todos.models import Todo
+ 
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+ 
+    class Meta:
+        model = Todo
+        fields = ('url', 'id', 'created', 'name', 'user')
+        extra_kwargs = {
+            'url': {
+                'view_name': 'todos:todo-detail',
+            }
+        }
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from todos.models import Todo
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+ 
+from todos.serializers import TodoSerializer 
+ 
+class TodoList(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+ 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+ 
+ 
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+ 
+    def get_queryset(self):
+        return Todo.objects.all().filter(user=self.request.user)
 
 # Create your views here.
